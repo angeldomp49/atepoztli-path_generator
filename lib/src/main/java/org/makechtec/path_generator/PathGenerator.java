34d7312c8094ca599632b2cc6a80c3d1857657bc
Angel Dominguez <angeldomp49@gmail.com>
2023-03-19 14:47:07 -0600

    first commit

---
+++ lib/src/main/java/org/makechtec/path_generator/PathGenerator.java
@@ -0,0 +1,33 @@
+package org.makechtec.path_generator;
+
+import java.net.URISyntaxException;
+import java.util.logging.Logger;
+
+public class PathGenerator {
+
+    private static final Logger LOG = Logger.getLogger(PathGenerator.class.getName());
+
+    public String jarPath(Class<?> mainClass) throws URISyntaxException {
+        return mainClass
+                .getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .toURI()
+                .getPath();
+    }
+
+    public String resourcePath(Class<?> mainClass, String resource) {
+
+        var urlResource =
+                mainClass.getClassLoader()
+                    .getResource(resource);
+
+        if(urlResource == null) {
+            LOG.warning("Could not find resource in PathGenerator.resourcePath(Class<?> mainClass, String resource)");
+            return "";
+        }
+
+        return urlResource.getPath();
+    }
+
+}
